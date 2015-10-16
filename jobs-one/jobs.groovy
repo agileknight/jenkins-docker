c31c1986770fe7dd76a7b8a3f99eaaeee965cc26
Philipp Meier <philipp.meier@codecentric.de>
2015-10-16 09:32:31 +0200

    add job to be deleted after being inactive

--- jobs-one/jobs.groovy
+++ jobs-one/jobs.groovy
@@ -9,3 +9,16 @@ job('DSL-Tutorial-1-Test') {
         maven('-e clean test')
     }
 }
+
+job('DSL-Tutorial-1-Test-Old') {
+    scm {
+        git('git://github.com/jgritman/aws-sdk-test.git')
+    }
+    triggers {
+        scm('*/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
+
